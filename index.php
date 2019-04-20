0fb1438b2f9ba103e65cb567f71c8e9bca70a6d0
Иван Гартман <senior.gartman@yandex.ru>
2019-04-20 13:16:05 +0700

    Работа с датами

--- index.php
+++ index.php
@@ -9,37 +9,43 @@ $lots = [
         'name' => '2014 Rossignol District Snowboard',
         'category' => 'Доски и лыжи',
         'price' => 10999,
-        'picture_url' => 'img/lot-1.jpg'
+        'picture_url' => 'img/lot-1.jpg',
+        'time' => 'tomorrow'
     ],
     [
         'name' => 'DC Ply Mens 2016/2017 Snowboard',
         'category' => 'Доски и лыжи',
         'price' => 159999,
-        'picture_url' => 'img/lot-2.jpg'
+        'picture_url' => 'img/lot-2.jpg',
+        'time' => 'tomorrow'
     ],
     [
         'name' => 'Крепления Union Contact Pro 2015 года размер L/X',
         'category' => 'Крепления',
         'price' => 8000,
-        'picture_url' => 'img/lot-3.jpg'
+        'picture_url' => 'img/lot-3.jpg',
+        'time' => 'tomorrow'
     ],
     [
         'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
         'category' => 'Ботинки',
         'price' => 10999,
-        'picture_url' => 'img/lot-4.jpg'
+        'picture_url' => 'img/lot-4.jpg',
+        'time' => 'tomorrow'
     ],
     [
         'name' => 'Куртка для сноуборда DC Mutiny Charocal',
         'category' => 'Одежда',
         'price' => 7500,
-        'picture_url' => 'img/lot-5.jpg'
+        'picture_url' => 'img/lot-5.jpg',
+        'time' => 'tomorrow'
     ],
     [
         'name' => 'Маска Oakley Canopy',
         'category' => 'Разное',
         'price' => 5400,
-        'picture_url' => 'img/lot-6.jpg'
+        'picture_url' => 'img/lot-6.jpg',
+        'time' => 'tomorrow'
     ]
 ];
 function price_format (float $price): string
@@ -48,6 +54,26 @@ function price_format (float $price): string
     $price = number_format($price, 0, '', ' ');
 
     return $price;
+}
+date_default_timezone_set("Asia/Novosibirsk");
+function timer($time)
+{
+    $now = date_create();
+    $time = date_create($time);
+    $interval = date_diff($now, $time);
+
+    return $interval->format('%H:%I');
+}
+function timer_finishing($time)
+{
+    $time = strtotime($time);
+    if (($time - time()) <= 3600) {
+
+        return true;
+    } else {
+
+        return false;
+    }
 }
 $page_content = include_template('index.php', [
     'categories' => $categories,
